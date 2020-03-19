08bcf0094d7fd7b70d6d05473e2f70c1ccd0ef63
Kevin Chipana Chucare <kvn.chipana@gmail.com>
2020-03-18 22:34:46 -0500

    Agregando autoslider

--- autowidth.js
+++ autowidth.js
@@ -11,17 +11,66 @@ function updateWidth(pageWitdh, margen){
 }
 
 
+
+let autoSlider;
+var paso = 1;
+
+function start(pageWitdh){
+  if(autoSlider) clearInterval(autoSlider);
+  autoSlider = setInterval(function(){scrollStep(pageWitdh)}, 3000);
+}
+
+function stop(){
+  if(autoSlider) clearInterval(autoSlider);
+  let cards = document.querySelectorAll(".slider-card ");
+  let cantCards = cards.length;
+  paso = cantCards + 100; /* 100 solo es un valor cualquiera, para romper la condicional de scrollStep() */ 
+}
+
+function scrollStep(pageWitdh){
+  let bodyWidth = document.querySelector("#slider-container");
+  let step = (bodyWidth.offsetWidth / pageWitdh);
+  let cards = document.querySelectorAll(".slider-card ");
+  let cantCards = cards.length;
+
+  let slider = document.querySelector("#slider");
+
+  if(paso > cantCards - pageWitdh){
+    slider.scrollBy(-(cantCards*step + 1000), 0)
+    paso=1;
+    console.log(paso);
+  }else{
+    slider.scrollBy(step, 0);
+    paso++;
+    console.log(paso);
+  }
+
+  console.log("hola")
+
+}
+
+function breakPoints(){
+if(window.matchMedia("(max-width: 991px)").matches){
+    updateWidth(2, 5);
+    stop();
+    start(2);    
+}
+
+if(window.matchMedia("(max-width: 500px)").matches){
+    updateWidth(1, 0);
+    stop();
+    start(1);
+}
+if(window.matchMedia("(min-width: 992px)").matches){
+    updateWidth(3, 10);
+    stop();
+    start(3);
+}
+}
+
 window.addEventListener("resize", ()=>{
-    
-
-    if(window.matchMedia("(max-width: 991px)").matches){
-                    updateWidth(2, 5);  
-    }
-
-    if(window.matchMedia("(max-width: 500px)").matches){
-                    updateWidth(1, 0);
-    }
-    if(window.matchMedia("(min-width: 992px)").matches){
-                    updateWidth(3, 10);
-    }
+  breakPoints();
 })
+
+breakPoints();
+
